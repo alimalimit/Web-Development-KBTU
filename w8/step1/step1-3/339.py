8aed90ba2fc9ec31f45fa286957237795631a0d2
alimalimit <47515924+alimalimit@users.noreply.github.com>
2020-03-21 21:22:30 +0600

    Add files via upload

---
+++ w8/step1/step1-3/339.py
@@ -0,0 +1,9 @@
+a = int(input())
+#i = 2
+#while a % i != 0:  
+   # i += 1
+#print(i)
+for i in range(2,a+1):
+	if a%i==0:
+		print(i)
+	break
---
+++ w8/step1/step1-3/342.py
@@ -0,0 +1,5 @@
+sum=0;
+for x in range(0,100):
+	sum+= int(input())
+
+print(sum)
