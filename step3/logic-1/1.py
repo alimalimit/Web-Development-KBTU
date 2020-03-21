eb02bf3619a94b4449672795a7e3a181bb4a6800
alimalimit <47515924+alimalimit@users.noreply.github.com>
2020-03-21 21:05:03 +0600

    Delete 1.py

--- step3/logic-1/1.py
+++
@@ -1,7 +0,0 @@
-def cigar_party(cigars, is_weekend):
-  if is_weekend==True and cigars>=40:
-    return True
-  elif cigars>=40 and cigars<=60 and is_weekend==False:
-    return True
-  else:
-    return False
