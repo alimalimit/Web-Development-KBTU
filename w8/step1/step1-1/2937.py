7f088695b2b1caadaba5d33723cea1d2cf537a7e
alimalimit <47515924+alimalimit@users.noreply.github.com>
2020-03-21 20:53:09 +0600

    Add files via upload

---
+++ w8/step1/step1-1/2937.py
@@ -0,0 +1,6 @@
+a=int(input())
+
+c=a+1
+d=a-1
+print("The next number for the number"+ " "+str(a) + " " +"is"+ " "+str(c) + ".")
+print("The previous number for the number"+ " "+ str(a) + " " + "is"+ " " + str(d) + ".")
